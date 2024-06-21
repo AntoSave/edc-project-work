e2eda62bd651c245d5f514efac4e718698f1d66f
Antonio Langella <a.langella31@studenti.unisa.it>
2024-06-21 18:22:48 +0200

    Import model in lqi_controller

--- lqi_controller.m
+++ lqi_controller.m
@@ -1,6 +1,8 @@
-%% 
-sys = ss(A,B,C,D);
+%% Import model
+load("pololu_37D_pos_second_order", "G_pos");
+sys = G_pos;
 
+%% Controller tuning
 Qu = 6e-6;
 Qx = [10 0 0;0 0.1 0;0 0 1];
 [K,S,e] = lqi(sys,Qx,Qu,0)
