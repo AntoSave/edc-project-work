343deabfddd51916333b11d02a3df7827d4da71f
Poggi28 <paolinosasa@gmail.com>
2024-06-23 20:52:37 +0200

    [ADD] Added state feedback incremental algorithm

---
+++ lqi_controller_incremental.m
@@ -0,0 +1,25 @@
+%% Import model
+load("pololu_37D_pos_second_order", "G_pos");
+sys = G_pos;
+Ts=0.005;
+
+%% Controller tuning
+% 
+% z_dot=y-r
+A_ext = [sys.A [0;0]; -sys.C 0];
+B_ext = [sys.B; 0];
+% sys_ext = ss(A_ext, B_ext, sys.C, sys.D)
+
+Qu = 0.001;
+Qx = [0.1 0 0; 0 10 0; 0 0 0.05];
+[K,S,e] = lqrd(A_ext, B_ext, Qx, Qu, 0, Ts) 
+% [K,S,e] = lqr(A_ext, B_ext, Qx, Qu, 0)
+
+%% Discrete
+sys_dis = c2d(sys,Ts)
+A_ext = [sys_dis.A [0;0]; -sys_dis.C 0];
+B_ext = [sys_dis.B; 0];
+
+Qu = 0.01;
+Qx = [0.1 0 0; 0 10 0; 0 0 0.01];
+[K,S,e] = dlqr(A_ext, B_ext, Qx, Qu, 0)
