220fa88fe99f5aeb47b0cdbfcaf51be20e60df30
Poggi28 <paolinosasa@gmail.com>
2024-06-30 10:55:20 +0200

    [ADD] Added a new current controller

--- torque_control/torque_PI_integral_algorithm.m
+++ torque_control/torque_PI_integral_algorithm.m
@@ -13,20 +13,22 @@ G = 1/(s*L+R);
 
 %% Controller tuning
 % rltool
-C = 0.45966*(s+1478)/s;
-
+% C = 0.45966*(s+1478)/s;
+C=68*(1+0.00067*s)/s;
 % Sampling time
 bode(feedback(C*G,1)) % Closed loop bandwidth of the system is 200 rad/s
-W_omega3 = 200;
+% W_omega3 = 200;
+W_omega3 = 20;
 omega_s_min = 5*W_omega3;
 Ts_max = 2*pi/omega_s_min;
 
 Ts = 0.005;
+pid(C)
 
 %% Integral Algorithm
 z = tf('z');
 Cz=c2d(C,Ts,'tustin')
-Kp=0.45966
-Ki=679.4
+Kp=2.91
+Ki=35
 Ti=1/(Ki/Kp)
 pippo=Kp+Ki*Ts*0.5*(z+1)/(z-1)
