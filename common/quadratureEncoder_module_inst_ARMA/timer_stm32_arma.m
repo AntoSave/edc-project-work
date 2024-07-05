2f89d42a71ead9a2fad67bf282723d4e2bfb3a0e
AntoSave <kiraanto@hotmail.it>
2024-07-05 22:23:16 +0200

    [ADD] ARMA instant speed module

---
+++ common/quadratureEncoder_module_inst_ARMA/timer_stm32_arma.m
@@ -0,0 +1,10 @@
+classdef timer_stm32_arma < int8
+   enumeration
+      TIM_1(1), 
+      TIM_4(4)
+   end
+end
+
+% GPL - Gianluca Canzolino - Master Student of Computer Engineering - UNISA
+% GPL - Giuseppe Gambardella - Master Student of Computer Engineering - UNISA
+% GPL - Alberto Provenza - Master Student of Computer Engineering - UNISA
