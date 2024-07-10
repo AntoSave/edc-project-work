671a49761b49f32cab84b31feecfeb9256b8c9f6
AntoSave <kiraanto@hotmail.it>
2024-07-10 18:16:01 +0200

    [REF]Admittance controller

--- admittance_control/admittance_controller.m
+++ admittance_control/admittance_controller.m
@@ -1,6 +1,17 @@
+% Incremental controller
+k = [6.3096 302.9280 26.1428];
+%% Admittance filter damped
 Md = 0.005;
 KD = 0.02;
 KP = 0.02;
+
+A=tf([1],[Md,KD,KP]);
+A_discr=c2d(A,0.005,'Tustin');
+
+%% Admittance filter springy
+Md = 0.002;
+KD = 0.003;
+KP = 0.01;
 
 A=tf([1],[Md,KD,KP]);
 A_discr=c2d(A,0.005,'Tustin');
