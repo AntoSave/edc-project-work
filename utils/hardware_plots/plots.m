6b20b147fb71c73ed6971749a898cbd41e90d7dc
AntoSave <kiraanto@hotmail.it>
2024-07-10 10:44:46 +0200

    [ADD] position hardware plots

--- utils/hardware_plots/plots.m
+++ utils/hardware_plots/plots.m
@@ -1,3 +1,23 @@
+%% Position step
+load('position_step')
+figure
+hold on
+plot(data{1}.Values)
+plot(data{2}.Values)
+xlim([0,1.5])
+ylabel('Position [deg]')
+title('Position controller step response')
+legend('r','y')
+%% Position stairs
+load('position_stair')
+figure
+hold on
+plot(data{1}.Values)
+plot(data{2}.Values)
+xlim([0,12])
+ylabel('Position [deg]')
+title('Position controller stair of steps response')
+legend('r','y')
 %% Torque step
 load('torque_step')
 figure
